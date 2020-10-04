e2fa14d6dc48409ee0d59e3db67eb5ad62dd325b
rong1002 <borong1002@gmail.com>
2020-10-04 21:32:06 +0800

    Initial Commit

---
+++ IOS-HW1/IOS_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW1App.swift
+//  IOS-HW1
+//
+//  Created by Lin Bo Rong on 2020/10/4.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-HW1Tests/IOS_HW1Tests.swift
@@ -0,0 +1,33 @@
+//
+//  IOS_HW1Tests.swift
+//  IOS-HW1Tests
+//
+//  Created by Lin Bo Rong on 2020/10/4.
+//
+
+import XCTest
+@testable import IOS_HW1
+
+class IOS_HW1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
